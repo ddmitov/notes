6814cef414ed1de913cb14a74991e87179a72cda
Dimitar D. Mitov <ddmitov@gmail.com>
2025-04-18 19:50:27 +0300

    code update

--- programming_languages/python3/minio.py
+++ programming_languages/python3/minio.py
@@ -14,3 +14,23 @@ minio_local_client = Minio(
 # Create bucket if it does not exist:
 if not minio_local_client.bucket_exists('BUCKET'):
     minio_local_client.make_bucket('BUCKET')
+
+
+# Object storage file cleaner:
+def file_cleaner(
+    client:      Minio,
+    bucket_name: str,
+    prefix:      str
+) -> True:
+    files_to_delete = client.list_objects(
+        bucket_name,
+        prefix    = prefix,
+        recursive = True
+    )
+
+    for file_object in files_to_delete:
+        client.remove_object(bucket_name, file_object.object_name)
+
+        print(str(file_object.object_name), flush=True)
+
+    return True
