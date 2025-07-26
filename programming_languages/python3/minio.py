eea48f1c0ce1fadd6e68010dc5f5adc097f07fa5
Dimitar D. Mitov <ddmitov@gmail.com>
2025-07-26 13:04:12 +0300

    code update

--- programming_languages/python3/minio.py
+++ programming_languages/python3/minio.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
-from minio import Minio
-
+import os
+from   pathlib import Path
+from   minio   import Minio
 
 # MinIO client:
 minio_local_client = Minio(
@@ -32,5 +33,39 @@ def file_cleaner(
         client.remove_object(bucket_name, file_object.object_name)
 
         print(str(file_object.object_name), flush=True)
+
+    return True
+
+
+def file_uploader(
+    object_storage_client: Minio,
+    bucket_name:           str,
+    prefix:                str,
+    root_directory:        str,
+    file_paths:            list
+) -> True:
+    file_number = 0
+
+    for file_path in file_paths:
+        file_number += 1
+
+        object_name = str(file_path).replace(f'{root_directory}/', '')
+
+        try:
+            object_storage_client.fput_object(
+                bucket_name,
+                prefix + '/' + object_name,
+                file_path,
+                part_size = 100 * 1024 * 1024 # 100 MB
+            )
+
+            message = (
+                f'{str(file_number)}/{str(len(file_paths))} - {object_name}'
+            )
+
+            print(message, flush=True)
+
+        except Exception as exception:
+            print(exception, flush=True)
 
     return True
